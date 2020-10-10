f61b39c1fd8b2a7a64cc449cb7cd65a90b988b95
laljyskebank <35138247+laljyskebank@users.noreply.github.com>
2020-10-10 08:09:29 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { KirbyModule } from '@kirbydesign/designsystem';
+import { HomeModule } from './home/home.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, KirbyModule, HomeModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
