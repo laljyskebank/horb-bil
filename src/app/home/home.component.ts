edcfbc7afe2449c91df592829f21e98ce7182edb
Nicolaj Lindstrøm <nli@jyskebank.dk>
2020-10-20 12:30:02 +0200

    Oprettelse af seks skærmbilleder

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  buttonSize = 'md';
 
+  data = [
+    1600.9,
+    1710.5,
+    1060.4,
+    1290.2,
+    1440.0,
+    1460.0,
+    1350.6,
+    1480.5,
+    1800.4,
+    1940.1,
+    1950.6,
+    1700.4,
+    1600.9,
+    1710.5,
+    1060.4,
+    1290.2,
+    1440.0,
+    1460.0,
+    1350.6,
+    1480.5,
+    1800.4,
+    1940.1,
+    1950.6,
+    1700.4,
+  ];
+  constructor() {}
   ngOnInit(): void {}
 }
